097abcc730f196d0a24f54ce0805f717b989f9d5
michaeladilulo <michaeladilulo@gmail.com>
2022-10-22 02:40:06 -0600

    Results show when you click on the op buttons

--- force-app/main/default/lwc/calculator/calculator.js
+++ force-app/main/default/lwc/calculator/calculator.js
@@ -2,7 +2,9 @@ import { LightningElement } from 'lwc';
 
 export default class Calculator extends LightningElement {
 firstNumber;
+calculating = false;
 secondNumber;
+operator;
 result;
 
 handleFirstChange(event) {
@@ -14,21 +16,29 @@ handleSecondChange(event) {
 }
 
 handleAddNums() {
+    this.operator = '+';
+    this.calculating = true;
     this.result = this.firstNumber + this.secondNumber;
     console.log('Add: ', parseInt(this.result));
 }
 
 handleSubtractNums() {
+    this.operator = '-';
+    this.calculating = true;
     this.result = this.firstNumber - this.secondNumber;
     console.log('Subtract: ', parseInt(this.result));
 }
 
 handleMultiplyNums() {
+    this.operator = '*';
+    this.calculating = true;
     this.result = this.firstNumber * this.secondNumber;
     console.log('Multiply: ', parseInt(this.result));
 }
 
 handleDivideNums() {
+    this.operator = '/';
+    this.calculating = true;
     this.result = this.firstNumber / this.secondNumber;
     console.log('Divide: ', parseInt(this.result));
 }
