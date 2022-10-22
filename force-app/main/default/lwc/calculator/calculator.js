89b2228b6e074c2f66174ce224074d21f64d4cee
michaeladilulo <michaeladilulo@gmail.com>
2022-10-22 17:09:10 -0600

    Blank slate before walkthrough. 90% completed

--- force-app/main/default/lwc/calculator/calculator.js
+++ force-app/main/default/lwc/calculator/calculator.js
@@ -1,41 +1,5 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, track } from 'lwc';
 
 export default class Calculator extends LightningElement {
-firstNumber;
-calculating = false;
-secondNumber;
-operator;
-result;
 
-handleFirstChange(event) {
-    this.firstNumber = parseInt(event.target.value)
-}
-
-handleSecondChange(event) {
-    this.secondNumber = parseInt(event.target.value)
-}
-
-handleAddNums() {
-    this.operator = '+';
-    this.calculating = true;
-    this.result = this.firstNumber + this.secondNumber;
-}
-
-handleSubtractNums() {
-    this.operator = '-';
-    this.calculating = true;
-    this.result = this.firstNumber - this.secondNumber;
-}
-
-handleMultiplyNums() {
-    this.operator = '*';
-    this.calculating = true;
-    this.result = this.firstNumber * this.secondNumber;
-}
-
-handleDivideNums() {
-    this.operator = '/';
-    this.calculating = true;
-    this.result = this.firstNumber / this.secondNumber;
-}
 }
