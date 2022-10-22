b984e768a59eddd2604e37809b9808d650ebe501
michaeladilulo <michaeladilulo@gmail.com>
2022-10-22 03:02:56 -0600

    Remove Console.log getting results in output

--- force-app/main/default/lwc/calculator/calculator.js
+++ force-app/main/default/lwc/calculator/calculator.js
@@ -19,27 +19,23 @@ handleAddNums() {
     this.operator = '+';
     this.calculating = true;
     this.result = this.firstNumber + this.secondNumber;
-    console.log('Add: ', parseInt(this.result));
 }
 
 handleSubtractNums() {
     this.operator = '-';
     this.calculating = true;
     this.result = this.firstNumber - this.secondNumber;
-    console.log('Subtract: ', parseInt(this.result));
 }
 
 handleMultiplyNums() {
     this.operator = '*';
     this.calculating = true;
     this.result = this.firstNumber * this.secondNumber;
-    console.log('Multiply: ', parseInt(this.result));
 }
 
 handleDivideNums() {
     this.operator = '/';
     this.calculating = true;
     this.result = this.firstNumber / this.secondNumber;
-    console.log('Divide: ', parseInt(this.result));
 }
 }
