540efb04bc97e3f5b5dc33d435ed930d121e43ac
michaeladilulo <michaeladilulo@gmail.com>
2022-10-21 23:12:57 -0600

    For:Each directive, used with For:Item

--- force-app/main/default/lwc/conditionalRendering/conditionalRendering.js
+++ force-app/main/default/lwc/conditionalRendering/conditionalRendering.js
@@ -3,6 +3,8 @@ import { LightningElement, track} from 'lwc';
 export default class ConditionalRendering extends LightningElement {
     @track dataDisplay = false;
 
+    @track cityList = ['Meridian', 'Boise', 'Nampa', 'Caldwell']
+
         handleDataDisplay(event) {
             this.dataDisplay = event.target.checked;
         }
