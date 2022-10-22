6c7c99c4891c104d9cf8ca6b3727f0aa5e94d9e3
michaeladilulo <michaeladilulo@gmail.com>
2022-10-22 17:42:11 -0600

    Created handlers for operations

--- force-app/main/default/lwc/calculator/calculator.js
+++ force-app/main/default/lwc/calculator/calculator.js
@@ -1,5 +1,27 @@
 import { LightningElement, track } from 'lwc';
 
 export default class Calculator extends LightningElement {
+@track currentResult;
+firstNumber;
+secondNumber;
 
+numberChangeHandler(event) {
+
+}
+
+addHandler() {
+
+}
+
+subHandler() {
+
+}
+
+multiplyHandler() {
+
+}
+
+divideHandler() {
+    
+}
 }
